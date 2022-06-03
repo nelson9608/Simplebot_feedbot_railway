583c18dc4f14b566b44abc9ac2e7ed9c492abf6a
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-06-03 18:23:20 -0400

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -2,27 +2,7 @@
 
 # configure the bot
 python3 -m simplebot init "$ADDR" "$PASSWORD"
-python3 -m simplebot -a "$ADDR" set_name "www"
-python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/mode" "command"
-python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/max_size" "1073741824"  # 1GB
-python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/delay" $DELAY
-python3 -m simplebot -a "$ADDR" db -s "simplebot_translator/filter_enabled" "no"
-
-# add the web_comress plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
-
-# add the web_search plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_search.py'); open('web_search.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
-
-# add the youtube plugin
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
-
-# add the encryption_error plugin to leverage key changes
-python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
-python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
+python3 -m simplebot -a "$ADDR" set_name "$BOTNAME"
 
 # add admin plugin
 if [ -n "$ADMIN" ]; then
