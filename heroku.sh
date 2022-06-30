e1f1e87ab9bf914922c5d51378d0cb44a49cb422
Nelson <104731311+nelson9608@users.noreply.github.com>
2022-06-30 16:59:53 -0400

    Update heroku.sh

--- heroku.sh
+++ heroku.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
 
-# configure the bot
-python3 -m simplebot init "$ADDR" "$PASSWORD"
-python3 -m simplebot -a "$ADDR" set_name "$BOTNAME"
+
+# add the encryption_error plugin to leverage key changes
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./encryption_error.py
 
 # add admin plugin
 if [ -n "$ADMIN" ]; then
